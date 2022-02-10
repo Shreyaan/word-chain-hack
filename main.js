fe354752689d0c72ff8f87eaa65b5f785949be86
Shreyaan Seth <Shreyaans41@gmail.com>
2022-02-10 19:00:21 +0530

    button

--- main.js
+++ main.js
@@ -1,52 +1,60 @@
-import { allWords } from './allWords.js';
+import {
+    allWords
+} from './allWords.js';
 
 let slider = document.getElementById("myRange");
 let output = document.getElementById("result");
 let sliderOutput = document.getElementById("myRangeLable");
-let letter= document.getElementById("inputText")
+let letter = document.getElementById("inputText")
 sliderOutput.innerHTML = slider.value;
 
+function button(element){
+let buttonHtml = `<button type="button" class="btn btn-dark"> ${element} </button> 
+`
+// navigator.clipboard.writeText(element);
 
-slider.oninput = function() {
+return buttonHtml
+}
+
+
+
+
+slider.oninput = function () {
     sliderOutput.innerHTML = this.value;
-    let letterVar= letter.value
+    let letterVar = letter.value
     let smallLetter = letterVar.toLowerCase()
-   
-        output.innerHTML = smallLetter
-      
 
-        const startsWith = allWords.filter((country) => country.startsWith(smallLetter));
-        let results = [];
-        for (const element of startsWith) {
-            if(element.length >= slider.value){
-                results.push(element)
-            }
+    output.innerHTML = smallLetter
+
+
+    const startsWith = allWords.filter((country) => country.startsWith(smallLetter));
+    let results = [];
+    for (const element of startsWith) {
+        if (element.length >= slider.value) {
+            results.push(element)
         }
+    }
+
+    output.innerHTML = ` Number of words = ${results.length} <br>`
+
+    if (results.length <= 0) {
         
-        output.innerHTML = ` Number of words = ${results.length} <br> <br> few random words  - <br><br> `
+        output.innerHTML += `lol no words `
 
-        if(results.length<=0){
-            output.innerHTML += `lol no words `
+    } else {
 
-        }
-        else{
+        if (results.length < 6) {
 
-            if(results.length<6){
+            output.innerHTML += ` <br> few random words  - <br><br> `
+            output.innerHTML += button(results)
+        } else {
+            output.innerHTML += ` <br> few random words  - <br><br> `
 
-                output.innerHTML += results
+            for (let i = 0; i < 6; i++) {
+                // output.innerHTML += `${results[Math.floor(Math.random() * results.length)]}, `
+                output.innerHTML += button(results[Math.floor(Math.random() * results.length)])
             }
-
-            else{
-                for (let i = 0; i < 6; i++) {
-                    output.innerHTML +=  `${results[Math.floor(Math.random() * results.length)]}, `
-                }
-                output.innerHTML +=  `${results[Math.floor(Math.random() * results.length)]} `
-            }
-            
-            
-            
-            
+           
         }
-        
-    
+    }
 }
